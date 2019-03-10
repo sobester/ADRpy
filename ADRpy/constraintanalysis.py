69151f24342c93b3060ccf4f8eadb210aed2e42e
Andras Sobester <sobester@live.com>
2019-03-10 19:53:03 +0000

    added ground effect factor example

--- ADRpy/constraintanalysis.py
+++ ADRpy/constraintanalysis.py
@@ -990,6 +990,30 @@ class AircraftConcept:
 
         where the 'oge' superscript denotes the 'out of ground effect' value.
 
+        **Example**::
+
+            import math
+            from ADRpy import constraintanalysis as co
+
+            designdef = {'aspectratio':8}
+            wingarea_m2 = 10
+            wingspan_m = math.sqrt(designdef['aspectratio'] * wingarea_m2)
+
+            for wingheight_m in [0.6, 0.8, 1.0]:
+
+                designdef['wingheightratio'] = wingheight_m / wingspan_m
+
+                aircraft = co.AircraftConcept(_, designdef, _, _)
+
+                print('h/b: ', designdef['wingheightratio'],
+                    ' Phi: ', aircraft.wigfactor())
+
+        Output::
+
+            h/b:  0.06708203932499368  Phi:  0.5353159851301115
+            h/b:  0.08944271909999159  Phi:  0.6719160104986877
+            h/b:  0.11180339887498948  Phi:  0.761904761904762
+
         """
         return ((16 * self.wingheightratio) ** 2) / (1 + (16 * self.wingheightratio) ** 2)
 
